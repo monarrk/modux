8b7f2f7f7861187b41c7b268c33dc40f7b6ef550
Skye Bleed <monarrk@aaathats3as.com>
2020-04-29 20:00:21 -0400

    Clean the code

--- src/ir.rs
+++ src/ir.rs
@@ -23,31 +23,18 @@ impl Ir {
         let mut emit_mut: Vec<char> = emit.chars().collect();
         let mut arg = 0usize;
 
-        let mut skip = false;
         for (i, c) in emit.chars().enumerate() {
-            if skip {
-                skip = false;
-                continue;
-            }
-            if c == '#' && emit.len() > i {
-                if emit.chars().nth(i + 1).unwrap() != '#' {
-                    println!("i @ : '{}'", emit_mut[i]);
-                    emit_mut.remove(i);
-                    let mut first = emit_mut.clone();
-                    let last = first.split_off(i);
-                    //println!("first: {}\nlast: {}", first.iter().collect::<String>(), last.iter().collect::<String>());
-                    let mut vec: Vec<char> = Vec::new();
-                    println!("{}", &what[starts[arg].parse::<usize>().expect("Failed to parse string")..ends[arg].parse::<usize>().expect("Failed to parse string")]);
-                    for c in (&what[starts[arg].parse::<usize>().expect("Failed to parse string")..ends[arg].parse::<usize>().expect("Failed to parse string")]).chars() {
-                        vec.push(c);
-                    }
-                    first = [first, vec[..].to_vec()].concat();
-                    emit_mut = [first, last.to_vec()].concat();
-                    arg += 1;
-                    skip = true;
-                } else {
-                    emit_mut.remove(i);
+            if c == '#' {
+                emit_mut.remove(i);
+                let mut first = emit_mut.clone();
+                let last = first.split_off(i);
+                let mut vec: Vec<char> = Vec::new();
+                for c in (&what[starts[arg].parse::<usize>().expect("Failed to parse string")..ends[arg].parse::<usize>().expect("Failed to parse string")]).chars() {
+                    vec.push(c);
                 }
+                first = [first, vec[..].to_vec()].concat();
+                emit_mut = [first, last.to_vec()].concat();
+                arg += 1;
             }
         }
 
